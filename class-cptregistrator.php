a13d5079bb39c7a41ce8b7b6b2a17508a69fa110
Analia Mok <analiam319@gmail.com>
2019-02-27 20:57:57 -0500

    Fixed most remaining phpcs issues

--- class-cptregistrator.php
+++ class-cptregistrator.php
@@ -17,25 +17,25 @@ use \CPT_Registrator\Base\CPT;
  * @package    CPT_Registrator
  * @author     Analia Mok
  */
-class CPTRegistrator
-{
-    /**
-     * Method to run on plugin activation.
-     *
-     * @return void
-     * @since 1.0.0
-     */
-    public static function activate() {
-        // Nothing...
-    }
+class CPTRegistrator {
 
-    /**
-     * Method to run on plugin deactivation.
-     *
-     * @return void
-     * @since 1.0.0
-     */
-    public static function deactivate() {
-        // Nothing...
-    }
+	/**
+	 * Method to run on plugin activation.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public static function activate() {
+		// Nothing...
+	}
+
+	/**
+	 * Method to run on plugin deactivation.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public static function deactivate() {
+		// Nothing...
+	}
 }
